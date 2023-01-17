b51d68c043cc826226d77e7d759c2ee0e64fbac2
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-01-17 23:33:59 +0700

    Advanced paging

--- module/restaurant/storage/list.go
+++ module/restaurant/storage/list.go
@@ -28,11 +28,26 @@ func (s *sqlStore) List(ctx context.Context,
 
 	// Check paging
 	paging.Fulfill()
-	db = db.Limit(paging.Limit).Offset((paging.Page - 1) * paging.Limit)
+	if v := paging.FakeCursor; v != "" {
+		uid, err := common.FromBase58(v)
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+		db = db.Where("id < ?", uid.GetLocalID())
+	} else {
+		offSet := (paging.Page - 1) * paging.Limit
+		db = db.Offset(offSet)
+	}
 
 	// Query data
-	if err := db.Find(&result).Error; err != nil {
-		return nil, err
+	if err := db.Limit(paging.Limit).Order("id desc").Find(&result).Error; err != nil {
+		return nil, common.ErrDB(err)
+	}
+
+	if len(result) > 0 {
+		last := result[len(result)-1]
+		last.Mask(true)
+		paging.NextCursor = last.FakeID.String()
 	}
 
 	return result, nil
