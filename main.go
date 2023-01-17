78307652f51eadb3d80d60c93f632315348014e4
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-01-17 13:10:19 +0700

    create middleware handler panic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"food_delivery/component/appctx"
+	"food_delivery/middleware"
 	restaurantginrestaurant "food_delivery/module/restaurant/transport/ginrestaurant"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -12,12 +13,13 @@ func main() {
 	dsn := "root:1234@tcp(127.0.0.1:3306)/food_delivery?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	db = db.Debug()
+
 	r := gin.Default()
 	if err != nil {
 		return
 	}
-
 	appCtx := appctx.NewAppCtx(db)
+	r.Use(middleware.Recover(appCtx))
 
 	restaurants := r.Group("/restaurants")
 	restaurants.POST("", restaurantginrestaurant.CreateRestaurant(appCtx))
