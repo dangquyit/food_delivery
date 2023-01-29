911e4558e313aa4d5c31d60dc87697b32a9ccb4d
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-01-29 23:47:14 +0700

    create user storage

---
+++ module/user/storage/find.go
@@ -0,0 +1,25 @@
+package userstorage
+
+import (
+	"food_delivery/common"
+	usermodel "food_delivery/module/user/model"
+	"golang.org/x/net/context"
+)
+
+func (s *sqlStorage) FindUser(ctx context.Context,
+	conditions map[string]interface{},
+	moreInfo ...string) (*usermodel.User, error) {
+	db := s.db.Begin()
+
+	for i := range moreInfo {
+		db = db.Preload(moreInfo[i])
+	}
+
+	var user usermodel.User
+
+	if err := db.Table(usermodel.User{}.TableName()).Where(conditions).First(&user).Error; err != nil {
+		return nil, common.ErrDB(err)
+	}
+
+	return &user, nil
+}
