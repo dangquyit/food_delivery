3273d2ab0da3eecb173c272f01a893ba0341ebc6
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-02-04 21:39:06 +0700

    add paging user like restaurant

--- module/restaurantlike/storage/list_user_like_restaurant.go
+++ module/restaurantlike/storage/list_user_like_restaurant.go
@@ -3,9 +3,13 @@ package restaurantlikestorage
 import (
 	"food_delivery/common"
 	restaurantlikemodel "food_delivery/module/restaurantlike/model"
+	"github.com/btcsuite/btcutil/base58"
 	"golang.org/x/net/context"
 	"log"
+	"time"
 )
+
+const timeFormat = "2006-01-02 15:04:05.999999999"
 
 func (s *sqlStorage) ListUserLikeRestaurant(ctx context.Context,
 	conditions map[string]interface{},
@@ -24,9 +28,23 @@ func (s *sqlStorage) ListUserLikeRestaurant(ctx context.Context,
 		}
 	}
 
-	db.Preload("User")
+	if err := db.Count(&paging.Total).Error; err != nil {
+		return nil, err
+	}
 
 	paging.Fulfill()
+	if f := paging.FakeCursor; f != "" {
+		timeCreated, err := time.Parse(timeFormat, string(base58.Decode(f)))
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+
+		db = db.Where("created_at < ?", timeCreated.Format(timeFormat))
+	} else {
+		db = db.Offset((paging.Page - 1) * paging.Limit)
+	}
+
+	db.Preload("User")
 
 	if err := db.Limit(paging.Limit).Order("created_at desc").Find(&result).Error; err != nil {
 		return nil, common.ErrDB(err)
@@ -40,6 +58,9 @@ func (s *sqlStorage) ListUserLikeRestaurant(ctx context.Context,
 		result[i].User.CreatedAt = v.CreatedAt
 		result[i].User.UpdatedAt = nil
 		users[i] = *result[i].User
+		if len(result)-1 == i {
+			paging.NextCursor = base58.Encode([]byte(v.CreatedAt.Format(timeFormat)))
+		}
 	}
 
 	return users, nil
